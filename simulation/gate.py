c6cb9e2cca184cc85be180ed3730ee05efd5b089
Zuriahn-Yun <Zuriahnyun1@gmail.com>
2025-08-11 14:05:24 -0700

    Create gate.py

---
+++ simulation/gate.py
@@ -0,0 +1,27 @@
+import numpy as np 
+import pandas as pd 
+
+# Decide the Representation
+def statevector(n):
+    size = 2 ** n 
+    
+
+# Define the gate mathematically
+
+def single_gate():
+    # 2x2 unitary matrix 
+    gate = [1,1,
+            1,1]
+
+def two_gate():
+    gate = [1,1,1,1,
+            1,1,1,1,
+            1,1,1,1,
+            1,1,1,1,]
+    
+# X gate = bit flip
+# H gate suportposition creator 
+# CNO = controlled NOT
+
+
+
